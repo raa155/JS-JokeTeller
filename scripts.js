ba841877a8917c755dddbbfc9145c6a1f9184a16
Roman <roman.a.abad@gmail.com>
2023-03-23 19:18:16 -0400

    Fetched Joke from Joke API | Implemented Voice functionality using VoiceRSS API | Added Event Listener to trigger a function that fetches a joke and reads it out loud to the user

--- scripts.js
+++ scripts.js
@@ -1,1 +1,49 @@
-// Voice
+
+const button =  document.getElementById('button')
+const audioElement = document.getElementById('audio')
+
+
+// Disable/Enable Button 
+function toggleButton() {
+   button.disabled = !button.disabled;
+}
+
+// Passing Joke to our VoiceRSS API 
+function tellMe(joke) {
+   VoiceRSS.speech({
+      key: '8c914f8cf79b43cca1fcbb2494f760a2',
+      src: joke,
+      hl: 'en-us',
+      v: 'Linda',
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false
+  });
+}
+
+// Get Jokes from Joke API
+
+async function getJokes() {
+   let joke = '';
+   const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+   try {
+      const response = await fetch(apiUrl);
+      const data = await response.json(); 
+      if (data.setup) {
+         joke = `${data.setup} ... ${data.delivery}`;
+      } else {
+         joke = data.joke;  
+      }
+      // Text-to-Speech
+      tellMe(joke);
+      // Disable Button
+      toggleButton();
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+// Event Listeners
+button?.addEventListener('click', getJokes);
+audioElement?.addEventListener('ended', toggleButton);
